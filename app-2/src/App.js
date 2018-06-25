78beb5b37c7e13489144b75eb0a5872ccd99652d
HavocPigeon <speeas@gmail.com>
2018-06-25 15:52:38 -0700

    final commit

--- app-2/src/App.js
+++ app-2/src/App.js
@@ -3,19 +3,28 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      arrayStrings: [
+        'spaghetti',
+        'ice-cream',
+        'sushi',
+        'bologna',
+      'cheese',
+      ]
+    }}
+    
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+       <h4> {this.state.arrayStrings.map(e => {
+         return <div>{e}</div>
+       })} </h4>
+               
       </div>
     );
   }
-}
+  }
 
 export default App;
