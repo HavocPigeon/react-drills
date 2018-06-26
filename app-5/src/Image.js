207a4335a44950a540eacd7c15e140a9eec8fac2
HavocPigeon <speeas@gmail.com>
2018-06-25 19:12:05 -0700

    final commit

---
+++ app-5/src/Image.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+export default class Image extends Component {
+    render(){
+        return(
+            <div className="App">
+            <img src="https://http.cat/100" alt="HTTP error code 100"/>
+            </div>
+        )
+    }
+}
